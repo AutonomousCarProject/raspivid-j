31e1186f63f2c35a945d7d13656b5d61fb4df017
edelmanjm <edelmanjm@users.noreply.github.com>
2018-07-23 12:50:43 -0700

    Change testing class to use enums
    Because it's a more difficult pass to test

--- test/src/PullData.java
+++ test/src/PullData.java
@@ -1,3 +1,8 @@
+import attributes.Attribute;
+import attributes.BoolAttribute;
+import attributes.IntAttribute;
+import attributes.StrAttribute;
+import attributes.enums.Exposure;
 import java.io.IOException;
 import java.util.Arrays;
 
@@ -7,7 +12,9 @@ public class PullData {
     try {
       byte[] a = new byte[100];
       int retcode;
-      Streamer s = new Streamer().start();
+      Streamer s = new Streamer(new Attribute[]{BoolAttribute.NOPREVIEW, IntAttribute.ISO.set(new
+          Integer[]{1000}), StrAttribute.OUTPUT.set(new String[]{"-"}), Exposure.EXPOSURE})
+          .start();
       try {
         Thread.sleep(1000);
       } catch (InterruptedException e) {
