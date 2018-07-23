e3d0902cf5f243b181b1110d670779871bc3897c
edelmanjm <edelmanjm@users.noreply.github.com>
2018-07-23 12:46:33 -0700

    Add additional enum modes

--- lib/src/attributes/enums/InitialState.java
+++ lib/src/attributes/enums/InitialState.java
@@ -4,28 +4,40 @@ import attributes.Attribute;
 import attributes.enums.InitialState.InitialStateMode;
 
 public enum InitialState implements Attribute<InitialStateMode> {
+  INITIAL_STATE(InitialStateMode.RECORD, "i");
 
-  // TODO
+  public final String shortname;
+  InitialStateMode v;
 
-  ;
+  InitialState(InitialStateMode defaultV, String shortname) {
+    this.v = defaultV;
+    this.shortname = shortname;
+  }
 
   @Override
   public InitialStateMode[] get() {
-    return new InitialStateMode[0];
+    return new InitialStateMode[]{v};
   }
 
   @Override
   public Attribute<InitialStateMode> set(InitialStateMode[] v) {
+    this.v = v[0];
     return this;
   }
 
   @Override
   public String getInvocationName() {
-    return null;
+    return shortname;
   }
 
   public enum InitialStateMode {
+    RECORD, PAUSE;
 
+    @Override
+    public String toString() {
+      return this.name().toLowerCase();
+    }
   }
-
 }
+
+
--- lib/src/attributes/enums/Mode.java
+++ lib/src/attributes/enums/Mode.java
@@ -4,28 +4,46 @@ import attributes.Attribute;
 import attributes.enums.Mode.ModeMode;
 
 public enum Mode implements Attribute<ModeMode> {
+  MODE(ModeMode.AUTO, "ex");
 
-  // TODO
+  public final String shortname;
+  ModeMode v;
 
-  ;
+  Mode(ModeMode defaultV, String shortname) {
+    this.v = defaultV;
+    this.shortname = shortname;
+  }
 
   @Override
   public ModeMode[] get() {
-    return new ModeMode[0];
+    return new ModeMode[]{v};
   }
 
   @Override
   public Attribute<ModeMode> set(ModeMode[] v) {
+    this.v = v[0];
     return this;
   }
 
   @Override
   public String getInvocationName() {
-    return null;
+    return shortname;
   }
 
   public enum ModeMode {
+    AUTO(0);
 
+    Integer code;
+
+    ModeMode(int code) {
+      this.code = code;
+    }
+
+    @Override
+    public String toString() {
+      return this.code.toString();
+    }
   }
-
 }
+
+
