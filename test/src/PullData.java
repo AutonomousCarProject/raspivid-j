9d66e0ff7c6717e4b37e105d91e31a5bd69c9e13
edelmanjm <edelmanjm@users.noreply.github.com>
2018-07-23 01:24:41 -0700

    Clean up basic tester

--- test/src/PullData.java
+++ test/src/PullData.java
@@ -1,10 +1,27 @@
 import java.io.IOException;
+import java.util.Arrays;
 
 public class PullData {
 
   public static void main(String[] args) {
     try {
-      new Streamer().start().frameStream();
+      byte[] a = new byte[100];
+      int retcode;
+      Streamer s = new Streamer().start();
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+      switch (retcode = s.frameStream().read(a)) {
+        case 0:
+        case -1:
+          System.out.println("Read failed with code " + retcode);
+          break;
+        default:
+          System.out.println(Arrays.toString(a));
+      }
+
     } catch (IOException e) {
       e.printStackTrace();
     }
